df69c02b74e66f9125b0c1bff6aa28af6cf540ed
Chad Skelton <cskeltonnews@gmail.com>
2020-07-10 16:19:16 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -21,8 +21,20 @@ url = "https://banweb3.kpu.ca/StudentRegistrationSsb/ssb/term/termSelection?mode
 
 html = requests.get(url)
 
+# htmlpage = html.content
+
+# soup = BeautifulSoup(htmlpage)
+
+# print soup
+
+url = "https://banweb3.kpu.ca/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_term=202030&startDatepicker=&endDatepicker=&uniqueSessionId=1ckwm1594421672965&pageOffset=0&pageMaxSize=10&sortColumn=subjectDescription&sortDirection=asc"
+
+html = requests.get(url)
+
 htmlpage = html.content
 
 soup = BeautifulSoup(htmlpage)
 
 print soup
+
+
