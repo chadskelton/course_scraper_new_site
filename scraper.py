97aad8a1b7da8ceb346dce46148171cafd9bdda5
Chad Skelton <cskeltonnews@gmail.com>
2020-07-11 16:56:44 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,7 +15,8 @@ import mechanize
 
 # BASIC SET UP
 br = mechanize.Browser()
-br.set_all_readonly(False)    # allow everything to be written to
+
+# br.set_all_readonly(False)    # allow everything to be written to
 br.set_handle_robots(False)   # no robots
 br.set_handle_refresh(False)  # can sometimes hang without this
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
