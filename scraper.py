f7e714a8e794b24abefd4a9bc7ae5172e95ce72c
Chad Skelton <cskeltonnews@gmail.com>
2020-07-11 11:11:06 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,8 +19,6 @@ import mechanize
 
 s = requests.Session()
 
-headers = "headers={'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'})
-
 url = "https://banweb3.kpu.ca/StudentRegistrationSsb/ssb/term/termSelection?mode=search"
 
 html = s.get(url, verify=False)
