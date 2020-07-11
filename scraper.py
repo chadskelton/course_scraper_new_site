acb7f18f20c00ec6f5a45f80e03cde9dfb3679b6
Chad Skelton <cskeltonnews@gmail.com>
2020-07-11 11:10:33 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,9 +19,11 @@ import mechanize
 
 s = requests.Session()
 
+headers = "headers={'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'})
+
 url = "https://banweb3.kpu.ca/StudentRegistrationSsb/ssb/term/termSelection?mode=search"
 
-html = s.get(url)
+html = s.get(url, verify=False)
 
 # htmlpage = html.content
 
@@ -31,11 +33,11 @@ html = s.get(url)
 
 url = "https://banweb3.kpu.ca/StudentRegistrationSsb/ssb/classSearch/classSearch"
 
-html = s.get(url)
+html = s.get(url, verify=False)
 
 url = "https://banweb3.kpu.ca/StudentRegistrationSsb/ssb/searchResults/searchResults?txt_term=202030&startDatepicker=&endDatepicker=&uniqueSessionId=1ckwm1594421672965&pageOffset=0&pageMaxSize=10&sortColumn=subjectDescription&sortDirection=asc"
 
-html = s.get(url)
+html = s.get(url, verify=False)
 
 htmlpage = html.content
 
